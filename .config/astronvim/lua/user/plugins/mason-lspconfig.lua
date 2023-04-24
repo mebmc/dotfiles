190668069d6d7736cf5f10798734b92930e3492c
Ben McDonald <ben@mebmc.uk>
2023-04-25 00:00:06 +0100

    fix(nvim): whitespace and disable hls

--- .config/astronvim/lua/user/plugins/mason-lspconfig.lua
+++ .config/astronvim/lua/user/plugins/mason-lspconfig.lua
@@ -1,43 +1,43 @@
 -- https://github.com/williamboman/mason-lspconfig.nvim
 return {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "neovim/nvim-lspconfig",
-    },
-    event = {
-        "BufReadPre",
-        "BufNewFile"
-    },
-    opts = {
-        automatic_installation = true,
-        ensure_installed = {
-            "awk_ls",
-            "bashls",
-            "cssls",
-            "dockerls",
-            "gopls",
-            "gradle_ls",
-            "graphql",
-            "hls",
-            "html",
-            "jsonls",
-            "lua_ls",
-            "marksman",
-            "pyright",
-            "robotframework_ls",
-            "rome",
-            "ruff_lsp",
-            "rust_analyzer",
-            "solidity",
-            "sqlls",
-            "tsserver",
-            "vimls",
-            "yamlls"
-        }
-    },
-    -- config = function()
-    --   require("mason").setup()
-    --   require("mason-lspconfig").setup()
-    -- end
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
+	opts = {
+		automatic_installation = true,
+		ensure_installed = {
+			"awk_ls",
+			"bashls",
+			"cssls",
+			"dockerls",
+			"gopls",
+			"gradle_ls",
+			"graphql",
+			-- "hls",
+			"html",
+			"jsonls",
+			"lua_ls",
+			"marksman",
+			"pyright",
+			"robotframework_ls",
+			"rome",
+			"ruff_lsp",
+			"rust_analyzer",
+			"solidity",
+			"sqlls",
+			"tsserver",
+			"vimls",
+			"yamlls",
+		},
+	},
+	-- config = function()
+	--   require("mason").setup()
+	--   require("mason-lspconfig").setup()
+	-- end
 }
