87648387262ff7b846c65ab915f560185e02c698
Ben McDonald <ben@mebmc.uk>
2023-05-19 19:06:13 +0100

    feat(nvim): add gopls lsp config

---
+++ .config/astronvim/lua/user/lsp/config/gopls.lua
@@ -0,0 +1,20 @@
+return {
+    settings = {
+        gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+                unusedparams = true,
+            },
+            hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+            },
+        }
+    }
+}
