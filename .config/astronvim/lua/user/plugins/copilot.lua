1e1b762c5643a2b0036e43b3305bb206818139ad
Ben McDonald <ben@mebmc.uk>
2023-05-19 18:18:02 +0100

    feat(nvim): add opts for copilot

--- .config/astronvim/lua/user/plugins/copilot.lua
+++ .config/astronvim/lua/user/plugins/copilot.lua
@@ -4,11 +4,33 @@ return {
     cmd = "Copilot",
     build = ":Copilot auth",
     opts = {
-        suggestion = {
-            enabled = false
-        },
         panel = {
-            enabled = false
+            enabled = true,
+            auto_refresh = false,
+            keymap = {
+                jump_prev = "[[",
+                jump_next = "]]",
+                accept = "<CR>",
+                refresh = "gr",
+                open = "<M-CR>"
+            },
+            layout = {
+                position = "bottom", -- | top | left | right
+                ratio = 0.4
+            },
+        },
+        suggestion = {
+            enabled = true,
+            auto_trigger = false,
+            debounce = 75,
+            keymap = {
+                accept = "<M-l>",
+                accept_word = false,
+                accept_line = false,
+                next = "<M-]>",
+                prev = "<M-[>",
+                dismiss = "<C-]>",
+            },
         },
         filetypes = {
             yaml      = false,
@@ -21,5 +43,7 @@ return {
             cvs       = false,
             ["*"]     = true,
         },
+        copilot_node_command = 'node', -- Node.js version must be > 16.x
+        server_opts_overrides = {},
     }
 }
