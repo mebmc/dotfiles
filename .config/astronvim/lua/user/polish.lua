d0a783ce64c326573e29052d32ef733312a93ecf
Ben McDonald <ben@mebmc.uk>
2023-05-19 18:22:23 +0100

    feat(nvim): add neovide transparency and format polish.lua

--- .config/astronvim/lua/user/polish.lua
+++ .config/astronvim/lua/user/polish.lua
@@ -1,36 +1,36 @@
-return {
-    -- This function is run last and is a good place to configuring
-    -- augroups/autocommands and custom filetypes also this just pure lua so
-    -- anything that doesn't fit in the normal config locations above can go here
-    polish = function()
-        local function yaml_ft(path, bufnr)
-            -- get content of buffer as string
-            local content = vim.filetype.getlines(bufnr)
-            if type(content) == "table" then
-                content = table.concat(content, "\n")
-            end
-
-            -- check if file is in roles, tasks, or handlers folder
-            local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
-            if path_regex and path_regex:match_str(path) then
-                return "yaml.ansible"
-            end
-            -- check for known ansible playbook text and if found, return yaml.ansible
-            local regex = vim.regex "hosts:\\|tasks:"
-            if regex and regex:match_str(content) then
-                return "yaml.ansible"
-            end
-
-            -- return yaml if nothing else
-            return "yaml"
+--     -- This function is run last and is a good place to configuring
+--     -- augroups/autocommands and custom filetypes also this just pure lua so
+--     -- anything that doesn't fit in the normal config locations above can go here
+return function()
+    local function yaml_ft(path, bufnr)
+        -- get content of buffer as string
+        local content = vim.filetype.getlines(bufnr)
+        if type(content) == "table" then
+            content = table.concat(content, "\n")
         end
 
-        vim.filetype.add {
-            extension = {
-                yml = yaml_ft,
-                yaml = yaml_ft,
-                kdl = "kdl"
-            }
-        }
+        -- check if file is in roles, tasks, or handlers folder
+        local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
+        if path_regex and path_regex:match_str(path) then
+            return "yaml.ansible"
+        end
+        -- check for known ansible playbook text and if found, return yaml.ansible
+        local regex = vim.regex "hosts:\\|tasks:"
+        if regex and regex:match_str(content) then
+            return "yaml.ansible"
+        end
+
+        -- return yaml if nothing else
+        return "yaml"
     end
-}
+
+    vim.filetype.add {
+        extension = {
+            yml = yaml_ft,
+            yaml = yaml_ft,
+            kdl = "kdl"
+        }
+    }
+
+    vim.g.neovide_transparency = 0.9
+end
