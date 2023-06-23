a55449bb6eabd5b8e60bf55248624d1986211414
Ben McDonald <ben@mebmc.uk>
2023-06-24 00:38:35 +0100

    fix(nvim): disable trouble auto-close

--- .config/astronvim/lua/user/plugins/trouble.lua
+++ .config/astronvim/lua/user/plugins/trouble.lua
@@ -4,7 +4,7 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
         position = "bottom",            -- position of the list can be: bottom, top, left, right
-        height = 10,                    -- height of the trouble list when position is top or bottom
+        height = 5,                     -- height of the trouble list when position is top or bottom
         width = 50,                     -- width of the list when position is left or right
         icons = true,                   -- use devicons for filenames
         mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
@@ -38,7 +38,7 @@ return {
         },
         indent_lines = true,               -- add an indent guide below the fold icons
         auto_open = true,                  -- automatically open the list when you have diagnostics
-        auto_close = true,                 -- automatically close the list when you have no diagnostics
+        auto_close = false,                -- automatically close the list when you have no diagnostics
         auto_preview = true,               -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
         auto_fold = false,                 -- automatically fold a file trouble list at creation
         auto_jump = { "lsp_definitions" }, -- for the given modes, automatically jump if there is only a single result
