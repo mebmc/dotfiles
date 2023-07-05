f9d428f9b593a143ce29a9ed861dc3071df20e7f
Ben McDonald <ben@mebmc.uk>
2023-07-05 21:06:46 +0100

    feat(nvim): tweak trouble

--- .config/astronvim/lua/user/plugins/trouble.lua
+++ .config/astronvim/lua/user/plugins/trouble.lua
@@ -3,16 +3,16 @@ return {
     "https://github.com/folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
-        position = "bottom",            -- position of the list can be: bottom, top, left, right
-        height = 10,                    -- height of the trouble list when position is top or bottom
-        width = 50,                     -- width of the list when position is left or right
-        icons = true,                   -- use devicons for filenames
-        mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
-        severity = nil,                 -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
-        fold_open = "",              -- icon used for open folds
-        fold_closed = "",            -- icon used for closed folds
-        group = true,                   -- group results by file
-        padding = true,                 -- add an extra new line on top of the list
+        position = "bottom",           -- position of the list can be: bottom, top, left, right
+        height = 10,                   -- height of the trouble list when position is top or bottom
+        width = 50,                    -- width of the list when position is left or right
+        icons = true,                  -- use devicons for filenames
+        mode = "document_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+        severity = nil,                -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
+        fold_open = "",             -- icon used for open folds
+        fold_closed = "",           -- icon used for closed folds
+        group = true,                  -- group results by file
+        padding = true,                -- add an extra new line on top of the list
         action_keys = {
             -- key mappings for actions in the trouble list
             -- map to {} to remove a mapping, for example:
@@ -37,7 +37,7 @@ return {
             next = "j"                     -- next item
         },
         indent_lines = true,               -- add an indent guide below the fold icons
-        auto_open = true,                  -- automatically open the list when you have diagnostics
+        auto_open = false,                 -- automatically open the list when you have diagnostics
         auto_close = false,                -- automatically close the list when you have no diagnostics
         auto_preview = true,               -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
         auto_fold = false,                 -- automatically fold a file trouble list at creation
