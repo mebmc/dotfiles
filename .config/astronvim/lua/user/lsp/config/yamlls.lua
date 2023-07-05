236e06df0fdef54a40217dfc4efa9bfe1eb3184c
Ben McDonald <ben@mebmc.uk>
2023-07-05 21:11:16 +0100

    feat(nvim): update yamlls lsp

--- .config/astronvim/lua/user/lsp/config/yamlls.lua
+++ .config/astronvim/lua/user/lsp/config/yamlls.lua
@@ -19,8 +19,28 @@ return {
                 ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "compose*.{yml,yaml}",
                 ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
                 ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-                ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}"
-            }
+                ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/*/tasks/*.{yml,yaml}"
+            },
+            -- customTags = {
+            --     "!Equals sequence",
+            --     "!FindInMap sequence",
+            --     "!GetAtt scalar",
+            --     "!GetAZs scalar",
+            --     "!ImportValue scalar",
+            --     "!Join sequence scalar",
+            --     "!Ref scalar",
+            --     "!Select sequence",
+            --     "!Split sequence",
+            --     "!Sub scalar",
+            --     "!And sequence",
+            --     "!Not sequence",
+            --     "!Equals sequence",
+            --     "!Sub sequence",
+            --     "!ImportValue scalar",
+            --     "!If sequence",
+            --     "!reference scalar",
+            --     "!reference sequence"
+            -- }
         }
     }
 }
