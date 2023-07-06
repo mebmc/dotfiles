f95973806aa022b6547ce8c8fe6eae5c969cf5ad
Ben McDonald <ben@mebmc.uk>
2023-07-07 00:51:28 +0100

    feat(nvim): add move plugin keybinds

--- .config/astronvim/lua/user/plugins/move.lua
+++ .config/astronvim/lua/user/plugins/move.lua
@@ -10,9 +10,31 @@ return {
     },
     lazy = false,
     keys = {
-        { "<S-Up>",   "<cmd>MoveLine(-1)<CR>",  mode = "n" },
-        { "<S-Down>", "<cmd>MoveLine(1)<CR>",   mode = "n" },
-        { "<S-Up>",   "<cmd>MoveBlock(-1)<CR>", mode = "x" },
-        { "<S-Down>", "<cmd>MoveBlock(1)<CR>",  mode = "x" },
+        { "<A-j>",      "<cmd>MoveLine(1)<CR>",    mode = "n" },
+        { "<A-k>",      "<cmd>MoveLine(-1)<CR>",   mode = "n" },
+        { "<A-h>",      "<cmd>MoveHChar(-1)<CR>",  mode = "n" },
+        { "<A-l>",      "<cmd>MoveHChar(1)<CR>",   mode = "n" },
+        { "<leader>wf", "<cmd>MoveWord(1)<CR>",    mode = "x" },
+        { "<leader>wb", "<cmd>MoveWord(-1)<CR>",   mode = "x" },
+        { "<A-j>",      "<cmd>MoveBlock(1)<CR>",   mode = "v" },
+        { "<A-k>",      "<cmd>MoveBlock(-1)<CR>",  mode = "v" },
+        { "<A-h>",      "<cmd>MoveHBlock(-1)<CR>", mode = "v" },
+        { "<A-l>",      "<cmd>MoveHBlock(1)<CR>",  mode = "v" },
     }
 }
+
+
+-- local opts = { noremap = true, silent = true }
+-- -- Normal-mode commands
+-- vim.keymap.set('n', '<A-j>', ':MoveLine(1)<CR>', opts)
+-- vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
+-- vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
+-- vim.keymap.set('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
+-- vim.keymap.set('n', '<leader>wf', ':MoveWord(1)<CR>', opts)
+-- vim.keymap.set('n', '<leader>wb', ':MoveWord(-1)<CR>', opts)
+--
+-- -- Visual-mode commands
+-- vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
+-- vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<CR>', opts)
+-- vim.keymap.set('v', '<A-h>', ':MoveHBlock(-1)<CR>', opts)
+-- vim.keymap.set('v', '<A-l>', ':MoveHBlock(1)<CR>', opts)
