0899a48807031acaf26b072a69ae4816c10a2b95
Ben McDonald <ben@mebmc.uk>
2023-11-27 17:56:40 +0000

    Remove ansible-stable-2.9 from yamlls schemas

--- .config/astronvim/lua/user/lsp/config/yamlls.lua
+++ .config/astronvim/lua/user/lsp/config/yamlls.lua
@@ -16,11 +16,12 @@ return {
                 url = "https://www.schemastore.org/api/json/catalog.json"
             },
             schemas = {
-                ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*.gitlab-ci.{yml,yaml}",
-                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "compose*.{yml,yaml}",
+                ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] =
+                "*.gitlab-ci.{yml,yaml}",
+                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
+                "compose*.{yml,yaml}",
                 ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
                 ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-                ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/*/tasks/*.{yml,yaml}"
             },
             customTags = {
                 "!Equals sequence",
