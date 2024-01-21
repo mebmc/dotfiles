8387cf5d8bd6ccc0bf44d7ad5bea0e0c563ac257
Ben McDonald <ben@mebmc.uk>
2024-01-21 20:56:48 +0000

    Add plugin for sourcegraph in astronvim

---
+++ .config/astronvim/lua/user/plugins/sourcegraph.lua
@@ -0,0 +1,16 @@
+-- Use your favorite package manager to install, for example in lazy.nvim
+--  Optionally, you can also install nvim-telescope/telescope.nvim to use some search functionality.
+return {
+    {
+        "sourcegraph/sg.nvim",
+        dependencies = { "nvim-lua/plenary.nvim", --[[ "nvim-telescope/telescope.nvim ]] },
+        lazy = false,
+
+        -- If you have a recent version of lazy.nvim, you don't need to add this!
+        build = "nvim -l build/init.lua",
+        keys = {
+            { "<leader>act", ":CodyToggle<cr>", desc = "Toggle Cody" }
+        },
+        opts = {}
+    },
+}
